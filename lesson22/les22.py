58891afba22e5b8f4cbb5d199b5e8f4574c7807f
AntonFromEarth <yatoha@gmail.com>
2021-10-22 08:40:29 +0300

    hw from 20.10.2021

---
+++ lesson22/les22.py
@@ -0,0 +1,64 @@
+
+def linear_shift(array: list, shift: int) -> list:
+    '''
+    array = [1, 2, 3, 4] shift = 1 => [0, 1, 2, 3]
+    array = [1, 2, 3, 4] shift = 2 => [0, 0, 1, 2]
+    array = [1, 2, 3, 4] shift = 3 => [0, 0, 0, 1]
+    '''
+    
+    temp = array[:-shift]
+    temp1 = [0 for i in range(shift)]
+    temp1.extend(temp)
+    return temp1 
+
+
+def circular_shift(array: list, shift: int) -> list:
+    '''
+    array = [1, 2, 3, 4] shift = 1 => [4, 1, 2, 3]
+    array = [1, 2, 3, 4] shift = 2 => [3, 4, 1, 2]
+    array = [1, 2, 3, 4] shift = 3 => [2, 3, 4, 1]
+    '''
+    
+    temp = []
+
+    for i in range(len(array)):
+        temp.append(array[i-shift])
+    return temp
+
+
+def nested_parentheses(incoming: str) -> bool:
+    '''
+    Функція отримує рядок, який складається тільки зі знаків "(" або ")"
+    Рядок вважається таким, що містить коректно вкладені скобки, якщо для
+    кожної скобки "(" існує відповідна ")". 
+    Функція повертає булевську змінну, яка показує, чи містить вхідний рядок
+    тільки правильно вкладені скобки - True, чи ні - False
+    incoming = "((())(())())" => True
+    incoming = "" => True
+    incoming = "(((())))" => True
+    incoming = "())" => False
+    incoming = "(()()(())" => False
+    '''
+
+    if incoming.count("(") == incoming.count(")") or incoming == "":
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    array = [1, 2, 3, 4]; shift = 2
+
+
+    print(linear_shift(array, shift))
+
+    print(circular_shift(array, shift))
+
+
+    incoming = "((())(())())"
+
+    #incoming = ""
+    #incoming = "(((())))"
+    #incoming = "())"
+    #incoming = "(()()(())"
+
+    print(nested_parentheses(incoming))
---
+++ lesson22/test_les22.py
@@ -0,0 +1,36 @@
+import unittest
+#import les22
+from les22 import linear_shift, circular_shift, nested_parentheses
+
+class TestLess22(unittest.TestCase):
+	def test_linear_shift(self):
+		array = [1, 2, 3, 4]; shift_1 = 1; shift_2 = 2
+		test_list=linear_shift(array, shift_1)
+		self.assertEqual(test_list, [0,1,2,3])
+		test_list=linear_shift(array, shift_2)
+		self.assertEqual(test_list, [0,0,1,2])
+
+
+	def test_circular_shift(self):
+		array = [1, 2, 3, 4]; shift_1 = 1; shift_2 = 2
+		test_list=circular_shift(array, shift_1)
+		self.assertEqual(test_list, [4,1,2,3])
+		test_list=circular_shift(array, shift_2)
+		self.assertEqual(test_list, [3,4,1,2])
+
+	def test_nested_parentheses(self):
+		incoming_t_1 = "((())(())())"
+		incoming_t_2 = ""
+		incoming_t_3 = "(((())))"
+		incoming_f_1 = "())"
+		incoming_f_2 = "("
+		self.assertTrue(nested_parentheses(incoming_t_1))
+		self.assertTrue(nested_parentheses(incoming_t_2))
+		self.assertTrue(nested_parentheses(incoming_t_3))
+		self.assertFalse(nested_parentheses(incoming_f_1))
+		self.assertFalse(nested_parentheses(incoming_f_2))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
